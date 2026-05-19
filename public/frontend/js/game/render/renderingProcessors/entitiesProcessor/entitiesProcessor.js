cdb3e3bcc8e94c0ad5e3bb9a530b0d290c19d553
Júlio <jc.5047792@gmail.com>
2026-05-19 19:13:24 -0400

    Diagnosticing data

--- public/frontend/js/game/render/renderingProcessors/entitiesProcessor/entitiesProcessor.js
+++ public/frontend/js/game/render/renderingProcessors/entitiesProcessor/entitiesProcessor.js
@@ -5,11 +5,14 @@ function entitiesProcessor() {
 
     const ctx = gameArea.context;
 
+const canvas = gameArea.canvas;
+const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+console.log("imageData.data: ", imageData.data)
     for (const id in gameState.entities) {
 
 	const entity = gameState.entities[id];
 
-	ctx.fillStyle = "red";
+	ctx.fillStyle = "green";
 
 	ctx.fillRect(entity.x, entity.y, 70, 50);
 
