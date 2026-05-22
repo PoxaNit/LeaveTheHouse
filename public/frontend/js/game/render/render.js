d005360bfb6d971c695ba2247470feb607c158d4
Júlio <jc.5047792@gmail.com>
2026-05-21 20:07:11 -0400

    Map Render Processor made, it's waiting for data be ready to test.

--- public/frontend/js/game/render/render.js
+++ public/frontend/js/game/render/render.js
@@ -1,4 +1,5 @@
 import entitiesProcessor from "./renderingProcessors/entitiesProcessor/entitiesProcessor.js";
+import mapProcessor from "./renderingProcessors/mapProcessor/mapProcessor.js";
 import {gameArea} from "../gameArea/gameArea.js";
 
 function render() {
@@ -6,6 +7,8 @@ function render() {
     let ctx = gameArea.context;
 
     ctx.clearRect(0, 0, gameArea.canvas.width, gameArea.canvas.height);
+
+    mapProcessor();
 
     entitiesProcessor();
 
--- public/frontend/js/game/render/renderingProcessors/mapProcessor/mapProcessor.js
+++ public/frontend/js/game/render/renderingProcessors/mapProcessor/mapProcessor.js
@@ -1,12 +1,12 @@
 import gameState from "../../../state/gameState/gameState.js";
 import {gameArea} from "../../../gameArea/gameArea.js";
 
-function setPixel(x, y, r, g, b, a) {
-    const index = (y * canvas.width + x) * 4;
-    data[index]     = r; // Red
-    data[index + 1] = g; // Green
-    data[index + 2] = b; // Blue
-    data[index + 3] = a; // Alpha (0-255)
+function hexToRgb(hex) {
+  const bigint = parseInt(hex.replace('#', ''), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return [r, g, b];
 }
 
 function mapProcessor () {
@@ -17,7 +17,37 @@ function mapProcessor () {
 
     const data = imageData.data;
 
-    
+    let n = 0; // imageData data index
+
+    const rows = gameState.mapData.rows;
+
+    for (let i = 0; i < rows.length; i++) {
+
+	for (let j = 0; j < rows[i].tiles.length; j++) {
+
+	    for (let k = 0; k < rows[i].tiles[j].rows.length) {
+
+		for (let z = 0; z < rows[i].tiles[j].rows[k].columns) {
+
+		    const column = rows[i].tiles[j].rows[k].columns[z]
+
+		    const rgb = hexToRgb(column.color);
+
+		    data[n] = rgb[0];
+
+		    data[n + 1] = rgb[1];
+
+		    data[n + 2] = rgb[2];
+
+		    n += 4;
+
+		}
+
+	    }
+
+	}
+
+    }
 
 }
 
