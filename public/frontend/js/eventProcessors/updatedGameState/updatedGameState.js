063a486062333ef93c7faf8302da7f47dc422551
Júlio <jc.5047792@gmail.com>
2026-05-26 11:32:05 -0400

    Some diagnosis made

--- public/frontend/js/eventProcessors/updatedGameState/updatedGameState.js
+++ public/frontend/js/eventProcessors/updatedGameState/updatedGameState.js
@@ -1,11 +1,12 @@
 import gameState from "../../game/state/gameState/gameState.js";
 
 function updatedGameState (payload) {
-console.log("updateGameState, payload: " + JSON.stringify(payload))
-    gameState.entities = payload.entities;
+setTimeout(() => console.log("updateGameState, payload: " + JSON.stringify(payload)), 5000)
 
     gameState.mapData = payload.mapData;
+    gameState.entities = payload.entities;
 
+setTimeout(() => console.log("payloadnow: ", gameState), 5000)
 }
 
 export default updatedGameState;
